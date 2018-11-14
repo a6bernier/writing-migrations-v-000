49914e29b239a5049b9b86a86e132237fb49d162
Andrew Bernier <a6bernier@gmail.com>
2018-11-14 18:07:28 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,15 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+  connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+
+
+  def change
+
+  end
+    
+    
 end
